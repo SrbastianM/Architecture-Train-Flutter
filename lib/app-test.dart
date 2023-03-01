64e8d324cc4d5890984fe60462a146b2a6bb828e
Tenki <srodriguezdev.l@gmail.com>
2023-03-01 15:58:35 -0500

    Actualmente se esta aprendiendo a profundidad los modelos y los problemas que conlleva realizar mal su creacion

--- lib/app-test.dart
+++ lib/app-test.dart
@@ -21,7 +21,7 @@ class _AppTestState extends State<AppTest> {
   }
 
   void loadData() async {
-    final responseCharacter = getCharacter();
+    var responseCharacter = await getCharacter() ?? [];
     if (responseCharacter == null) {
       return;
     }
@@ -33,6 +33,55 @@ class _AppTestState extends State<AppTest> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: character.length,
+        itemBuilder: (BuildContext context, int index) {
+          final userComment = Container(
+            margin: const EdgeInsets.only(top: 10.0, left: 20.0),
+            child: Text(
+              character[index].id.toString(),
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 13.0,
+                fontFamily: "Lato",
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          );
+          final userName = Container(
+            margin: const EdgeInsets.only(top: 20.0, left: 20.0),
+            child: Text(
+              character[index].name,
+              textAlign: TextAlign.left,
+              style: const TextStyle(fontSize: 18.0, fontFamily: "Lato"),
+            ),
+          );
+
+          final photo = Container(
+            margin: const EdgeInsets.only(
+              top: 20.0,
+              left: 20.0,
+            ),
+            width: 80.0,
+            height: 80.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(character[index].image),
+              ),
+            ),
+          );
+
+          final userInformation = Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [userName, userComment],
+          );
+          return Row(children: [photo, userInformation]);
+        },
+      ),
+    );
   }
 }
